b085d0ec647d289f4b3aac3be9cb52599f654206
tangwei <530959820@qq.com>
2017-11-24 09:25:41 +0800

    Initial Commit

---
+++ Veriface/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Veriface
+//
+//  Created by tang tang on 2017/11/24.
+//  Copyright © 2017年 tang tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
